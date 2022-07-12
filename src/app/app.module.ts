99d73722711be199683c7c1562c5db65f919c39b
Akhil7654 <msdcr88@gmail.com>
2022-07-13 00:20:21 +0530

    Hospital done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
+import { SearchdepartmentComponent } from './searchdepartment/searchdepartment.component';
+import { ViewdoctorsComponent } from './viewdoctors/viewdoctors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontpageComponent
+    FrontpageComponent,
+    SearchdepartmentComponent,
+    ViewdoctorsComponent
   ],
   imports: [
     BrowserModule,
